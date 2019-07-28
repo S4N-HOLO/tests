c3cc2d7b6dbe77da62cf4661c20972142311d7e8
S4N-HOLO <sc.fatman@gmail.com>
2019-07-28 23:18:53 +0300

    sadness

--- tests/main.cpp
+++ tests/main.cpp
@@ -10,9 +10,9 @@ int main()
 	string name = " Alex ";
 	ofstream output("alex.txt");
 	output << "yo " << name;
-	int a = 0;
-	ofstream output("a.txt");
-	output << a;
+	int a = 234340;
+	ofstream xoutput("a.txt");
+	xoutput << a;
 	int b = a;
 
 	return (0);
